dabf9b3ca4f546c6db98ea9ba20c26cea1a24c17
gaoweidong <511872270@qq.com>
2020-01-03 23:59:03 +0800

    bug fix

--- common/src/main/java/com/alibaba/datax/common/exception/CommonErrorCode.java
+++ common/src/main/java/com/alibaba/datax/common/exception/CommonErrorCode.java
@@ -8,8 +8,8 @@ import com.alibaba.datax.common.spi.ErrorCode;
 public enum CommonErrorCode implements ErrorCode {
 
     CONFIG_ERROR("Common-00", "您提供的配置文件存在错误信息，请检查您的作业配置 ."),
-    CONVERT_NOT_SUPPORT("Common-01", "同步数据出现业务脏数据情况，数据类型转换错误 ."),
-    CONVERT_OVER_FLOW("Common-02", "同步数据出现业务脏数据情况，数据类型转换溢出 ."),
+    CONVERT_NOT_SUPPORT("Common-01", "同步数据出现业务脏数据情况，数据类型转换错误."),
+    CONVERT_OVER_FLOW("Common-02", "同步数据出现业务脏数据情况，数据类型转换溢出."),
     RETRY_FAIL("Common-10", "方法调用多次仍旧失败 ."),
     RUNTIME_ERROR("Common-11", "运行时内部调用错误 ."),
     HOOK_INTERNAL_ERROR("Common-12", "Hook运行错误 ."),
